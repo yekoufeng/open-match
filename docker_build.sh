dec0e252f0f6445a484ac0524dbb3eb9e6de611d
Jin Qing <jinq0123@163.com>
2018-09-26 18:25:26 +0800

    Update docker_build.sh.

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+docker build . -f Dockerfile.backendapi -t registry.cn-shanghai.aliyuncs.com/jinq0123/openmatch-backendapi:dev
+docker build . -f Dockerfile.evaluator -t registry.cn-shanghai.aliyuncs.com/jinq0123/openmatch-evaluator:dev
 docker build . -f Dockerfile.frontendapi -t registry.cn-shanghai.aliyuncs.com/jinq0123/openmatch-frontendapi:dev
-
+docker build . -f Dockerfile.mmf -t registry.cn-shanghai.aliyuncs.com/jinq0123/openmatch-mmf:dev
 docker build . -f Dockerfile.mmforc -t registry.cn-shanghai.aliyuncs.com/jinq0123/openmatch-mmforc:dev
+
